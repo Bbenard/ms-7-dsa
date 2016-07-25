a3afd907141603d6954b14aa1ab7a40b83e65140
Guruben <benah1013@gmail.com>
2016-07-25 15:00:37 +0300

    solution find the highusing only the digit in the given number

--- next_higest/nextHighest.java
+++ next_higest/nextHighest.java
@@ -1,2 +1,29 @@
 // Given a number, find the next higher number using only the digits in the given number. 
 //For example if the given number is 1508, the next higher number with same digits is 8510.
+solution
+~~~java
+public class NewMain2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num=6789;
+        int i=String.valueOf(num).length();
+        int k=0;
+        int[]numbers=new int[i];
+        while (k< i) {
+            
+   numbers[k] = num % 10;
+    num = num / 10;
+    k++;
+}
+    
+     for (int digit :numbers ) {
+   System.out.print(digit );
+   } 
+        // TODO code application logic here
+    }
+    
+}
+
