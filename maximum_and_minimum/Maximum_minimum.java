e010661a7347e3b649778f5438adb75d53077b90
Guruben <benah1013@gmail.com>
2016-07-25 09:49:16 +0300

    solutiom on max minimum

--- maximum_and_minimum/Maximum_minimum.java
+++ maximum_and_minimum/Maximum_minimum.java
@@ -9,3 +9,20 @@
 // put the lowest in its array 
 // put the highest in its array
 // output the highest then output the lowest
+
+
+//solution for maximum
+```java
+import java.util.Arrays;
+public class sortt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] x = {45, 12, 9, 89,100 };
+        Arrays.sort(x);
+        System.out.println("maximu number " +x[x.length-1]);
+         System.out.println("minimum number " +x[0]);
+}
+}
